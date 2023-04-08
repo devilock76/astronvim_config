33abd4d773fe7f8209d97220fefeb3cb89238f83
Ken M <kmackenzie@cap-rx.com>
2023-04-08 20:43:36 +0000

    treesitter parsers to support nvim-test

--- plugins/user.lua
+++ plugins/user.lua
@@ -46,18 +46,18 @@ return {
     event = "VeryLazy",
     config = function()
       require("nvim-test").setup {
-        run = true,               -- run tests (using for debug)
-        commands_create = true,   -- create commands (TestFile, TestLast, ...)
-        filename_modifier = ":.", -- modify filenames before tests run(:h filename-modifiers)
-        silent = false,           -- less notifications
-        term = "terminal",        -- a terminal to run ("terminal"|"toggleterm")
+        run = true,                 -- run tests (using for debug)
+        commands_create = true,     -- create commands (TestFile, TestLast, ...)
+        filename_modifier = ":p:.", -- modify filenames before tests run(:h filename-modifiers)
+        silent = false,             -- less notifications
+        term = "terminal",          -- a terminal to run ("terminal"|"toggleterm")
         termOpts = {
-          direction = "vertical", -- terminal's direction ("horizontal"|"vertical"|"float")
-          width = 128,            -- terminal's width (for vertical|float)
-          height = 24,            -- terminal's height (for horizontal|float)
-          go_back = false,        -- return focus to original window after executing
-          stopinsert = "auto",    -- exit from insert mode (true|false|"auto")
-          keep_one = true,        -- keep only one terminal for testing
+          direction = "vertical",   -- terminal's direction ("horizontal"|"vertical"|"float")
+          width = 128,              -- terminal's width (for vertical|float)
+          height = 24,              -- terminal's height (for horizontal|float)
+          go_back = false,          -- return focus to original window after executing
+          stopinsert = "auto",      -- exit from insert mode (true|false|"auto")
+          keep_one = true,          -- keep only one terminal for testing
         },
         runners = {
           -- setup tests runners
@@ -74,6 +74,7 @@ return {
           typescriptreact = "nvim-test.runners.jest",
         }
       }
+      -- require("nvim-test.runners.pytest").setup {}
     end
   }
 }
