36a5ee29213742b37c1f5fb9040c28a6513def69
devilock76 <kmackenzie@cap-rx.com>
2024-01-24 13:59:49 -0500

    default installs in mason

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -13,7 +13,18 @@ return {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
-      -- ensure_installed = { "lua_ls" },
+      ensure_installed = {
+        "lua_ls",
+        "bashls",
+        "eslint",
+        "jedi_language_server",
+        "remark_ls",
+        "ruff_lsp",
+        "terraformls",
+        "tflint",
+        "tsserver",
+        "yamlls",
+      },
     },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
@@ -21,14 +32,31 @@ return {
     "jay-babu/mason-null-ls.nvim",
     -- overrides `require("mason-null-ls").setup(...)`
     opts = {
-      -- ensure_installed = { "prettier", "stylua" },
+      ensure_installed = {
+        "prettier",
+        "stylua",
+        "eslint_d",
+        "markdownlint",
+        "ruff",
+        "shellcheck",
+        "tflint",
+        "yamllint",
+        "markdownlint",
+        "prettier",
+        "prettierd",
+      },
     },
   },
   {
     "jay-babu/mason-nvim-dap.nvim",
     -- overrides `require("mason-nvim-dap").setup(...)`
     opts = {
-      -- ensure_installed = { "python" },
+      ensure_installed = {
+        "python",
+        "bash-debug-adapter",
+        "debugpy",
+        "js-debug-adapter",
+      },
     },
   },
 }
